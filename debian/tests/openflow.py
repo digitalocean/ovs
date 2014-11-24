b13955ddd7d447eae451fb064e31d77006b75c7e
James Page <james.page@ubuntu.com>
2014-11-24 14:41:55 +0000

    Import patches-unapplied version 2.3.0+git20140819-2ubuntu2 to ubuntu/vivid-proposed
    Imported using git-ubuntu import.
    
    Changelog parent: 8359670013facb34615ab9c7c167526e89348ce8
    
    New changelog entries:
      * Resolve DEP-8 test failures:
        - d/tests/openflow.py: Explicitly configure switches to use
          OpenFlow 1.0 protocol.
        - d/tests/control: Version dependency on mininet >= 2.2.0~
          to support specification of protocol version.
      * d/tests/openflow.py: format to flake8 standard.

--- debian/tests/openflow.py
+++ debian/tests/openflow.py
@@ -2,12 +2,11 @@ import unittest
 import logging
 from mininet.net import Mininet
 from mininet.node import OVSController, OVSKernelSwitch
-from mininet.cli import CLI
-from mininet.log import setLogLevel
 
 Switch = OVSKernelSwitch
 Controller = OVSController
 logging.basicConfig(level=logging.INFO)
+
 
 class BasicOpenflowTest(unittest.TestCase):
 
@@ -15,28 +14,28 @@ class BasicOpenflowTest(unittest.TestCase):
         logging.debug("Creating host h%s and add to net.", N)
         name = 'h%d' % N
         ip = '10.0.0.%d' % N
-        return self.net.addHost( name, ip=ip )
+        return self.net.addHost(name, ip=ip)
 
     def setUp(self):
-        self.net = Mininet( controller=Controller, switch=Switch)
+        self.net = Mininet(controller=Controller, switch=Switch)
 
         logging.info("Creating controllers")
-        c1 = self.net.addController( 'c1' )
+        self.net.addController('c1')
 
         logging.info("Creating switches")
-        s1 = self.net.addSwitch( 's1' )
-        s2 = self.net.addSwitch( 's2' )
+        s1 = self.net.addSwitch('s1', protocols="OpenFlow10")
+        s2 = self.net.addSwitch('s2', protocols="OpenFlow10")
 
         logging.info("Creating hosts (7 on each switch)")
-        hosts1 = [ self.addHost( n ) for n in 1, 2, 3, 4, 5, 6, 7 ]
-        hosts2 = [ self.addHost( n ) for n in 8, 9, 10, 11, 12, 13, 14 ]
+        hosts1 = [self.addHost(n) for n in (1, 2, 3, 4, 5, 6, 7)]
+        hosts2 = [self.addHost(n) for n in (8, 9, 10, 11, 12, 13, 14)]
 
         logging.info("Creating links")
         for h in hosts1:
-            s1.linkTo( h )
+            s1.linkTo(h)
         for h in hosts2:
-            s2.linkTo( h )
-        s1.linkTo( s2 )
+            s2.linkTo(h)
+        s1.linkTo(s2)
 
         logging.info("Starting network")
         self.net.start()
@@ -44,11 +43,14 @@ class BasicOpenflowTest(unittest.TestCase):
     def testPingAll(self):
         logging.info("Testing network")
         packetLoss = self.net.pingAll()
-        self.assertTrue(packetLoss == 0, "Packet loss during ping test %s" %packetLoss)
+        self.assertTrue(
+            packetLoss == 0,
+            "Packet loss during ping test %s" %
+            packetLoss)
 
     def testIPerfTCP(self):
         logging.info("Running TCP performance test")
-        results = self.net.iperf()
+        self.net.iperf()
 
     def testIPerfUDP(self):
         logging.info("Running UDP performance test")
@@ -60,5 +62,3 @@ class BasicOpenflowTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
