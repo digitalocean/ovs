ba5ae7472dd3e3289c72ccd170b27b9ad18f4e61
Johannes Erdfelt <johannes@erdfelt.com>
2018-09-11 08:06:36 -0700

    Import 2.10.0-0ubuntu2 from Ubuntu

--- debian/automake.mk
+++ debian/automake.mk
@@ -2,24 +2,11 @@ EXTRA_DIST += \
 	debian/changelog \
 	debian/compat \
 	debian/control \
-	debian/control.modules.in \
 	debian/copyright \
 	debian/copyright.in \
-	debian/dkms.conf.in \
 	debian/dirs \
-	debian/libopenvswitch.install \
-	debian/libopenvswitch-dev.install \
 	debian/openvswitch-common.dirs \
-	debian/openvswitch-common.docs \
 	debian/openvswitch-common.install \
-	debian/openvswitch-common.manpages \
-	debian/openvswitch-datapath-module-_KVERS_.postinst.modules.in \
-	debian/openvswitch-datapath-dkms.postinst \
-	debian/openvswitch-datapath-dkms.prerm \
-	debian/openvswitch-datapath-source.README.Debian \
-	debian/openvswitch-datapath-source.copyright \
-	debian/openvswitch-datapath-source.dirs \
-	debian/openvswitch-datapath-source.install \
 	debian/openvswitch-pki.dirs \
 	debian/openvswitch-pki.postinst \
 	debian/openvswitch-pki.postrm \
@@ -28,53 +15,35 @@ EXTRA_DIST += \
 	debian/openvswitch-switch.init \
 	debian/openvswitch-switch.install \
 	debian/openvswitch-switch.logrotate \
-	debian/openvswitch-switch.manpages \
 	debian/openvswitch-switch.postinst \
 	debian/openvswitch-switch.postrm \
-	debian/openvswitch-switch.template \
+	debian/openvswitch-switch.default \
 	debian/openvswitch-switch.links \
-	debian/openvswitch-test.dirs \
 	debian/openvswitch-test.install \
-	debian/openvswitch-test.manpages \
 	debian/openvswitch-testcontroller.README.Debian \
 	debian/openvswitch-testcontroller.default \
 	debian/openvswitch-testcontroller.dirs \
 	debian/openvswitch-testcontroller.init \
 	debian/openvswitch-testcontroller.install \
-	debian/openvswitch-testcontroller.manpages \
 	debian/openvswitch-testcontroller.postinst \
 	debian/openvswitch-testcontroller.postrm \
 	debian/openvswitch-vtep.default \
 	debian/openvswitch-vtep.dirs \
 	debian/openvswitch-vtep.init \
 	debian/openvswitch-vtep.install \
-	debian/openvswitch-vtep.manpages \
-	debian/ovn-central.dirs \
 	debian/ovn-central.init \
 	debian/ovn-central.install \
-	debian/ovn-central.manpages \
-	debian/ovn-central.postinst \
 	debian/ovn-central.postrm \
-	debian/ovn-central.template \
-	debian/ovn-controller-vtep.init \
-	debian/ovn-controller-vtep.install \
-	debian/ovn-controller-vtep.manpages \
+	debian/ovn-central.default \
 	debian/ovn-common.install \
-	debian/ovn-common.manpages \
 	debian/ovn-common.postinst \
 	debian/ovn-common.postrm \
-	debian/ovn-docker.install \
-	debian/ovn-host.dirs \
+    debian/ovn-docker.install \
 	debian/ovn-host.init \
 	debian/ovn-host.install \
-	debian/ovn-host.manpages \
-	debian/ovn-host.postinst \
 	debian/ovn-host.postrm \
-	debian/ovn-host.template \
-	debian/python-openvswitch.dirs \
-	debian/python-openvswitch.install \
+	debian/ovn-host.default \
 	debian/rules \
-	debian/rules.modules \
 	debian/ifupdown.sh \
 	debian/source/format
 
@@ -93,9 +62,9 @@ DIST_HOOKS += check-debian-changelog-version
 $(srcdir)/debian/copyright: AUTHORS.rst debian/copyright.in
 	$(AM_V_GEN) \
 	{ sed -n -e '/%AUTHORS%/q' -e p < $(srcdir)/debian/copyright.in;   \
-	  sed '34,/^$$/d' $(srcdir)/AUTHORS.rst |			   \
+	  sed '34,/^$$/d' $(srcdir)/AUTHORS.rst  |				   \
 		sed -n -e '/^$$/q' -e 's/^/  /p';			   \
 	  sed -e '34,/%AUTHORS%/d' $(srcdir)/debian/copyright.in;	   \
 	} > $@
 
-CLEANFILES += debian/copyright
+DISTCLEANFILES += debian/copyright
--- debian/ifupdown.sh
+++ debian/ifupdown.sh
@@ -29,8 +29,15 @@ if (ovs_vsctl --version) > /dev/null 2>&1; then :; else
     exit 0
 fi
 
-if /etc/init.d/openvswitch-switch status > /dev/null 2>&1; then :; else
-    /etc/init.d/openvswitch-switch start
+SERVICE_UNIT=/usr/lib/systemd/system/openvswitch-nonetwork.service
+if [ -f $SERVICE_UNIT ] && [ -x /usr/bin/systemctl ]; then
+    if ! systemctl --quiet is-active openvswitch-nonetwork.service; then
+        systemctl start openvswitch-nonetwork.service
+    fi
+else
+    if service openvswitch-switch status > /dev/null 2>&1; then
+        service openvswitch-switch start
+    fi
 fi
 
 if [ "${MODE}" = "start" ]; then
@@ -50,24 +57,24 @@ if [ "${MODE}" = "start" ]; then
                     "${IFACE}" ${IF_OVS_OPTIONS} \
                     ${OVS_EXTRA+-- $OVS_EXTRA}
 
-                ip link set "${IFACE}" up
+                ifconfig "${IFACE}" up
                 ;;
         OVSIntPort)
                 ovs_vsctl -- --may-exist add-port "${IF_OVS_BRIDGE}"\
                     "${IFACE}" ${IF_OVS_OPTIONS} -- set Interface "${IFACE}"\
                     type=internal ${OVS_EXTRA+-- $OVS_EXTRA}
 
-                ip link set "${IFACE}" up
+                ifconfig "${IFACE}" up
                 ;;
         OVSBond)
                 ovs_vsctl -- --fake-iface add-bond "${IF_OVS_BRIDGE}"\
                     "${IFACE}" ${IF_OVS_BONDS} ${IF_OVS_OPTIONS} \
                     ${OVS_EXTRA+-- $OVS_EXTRA}
 
-                ip link set "${IFACE}" up
+                ifconfig "${IFACE}" up
                 for slave in ${IF_OVS_BONDS}
                 do
-                    ip link set "${slave}" up
+                    ifconfig "${slave}" up
                 done
                 ;;
         OVSPatchPort)
